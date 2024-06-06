63130d5edeb97c22ecaaee0135ef02a2db9ddab4
0zodbek <ozpdbekov@gmail.com>
2024-06-06 21:36:08 +0500

    10 dars

---
+++ app.js
@@ -0,0 +1,41 @@
+//************************ 1-masala ************************
+
+// function arrkvadrat(arg){
+//     let a = arg ;
+//     let b = [] ;
+//     a.forEach(function(value){
+//         b.push(value * value)
+    
+//         return b ;
+//     })   
+//     console.log(b);
+// }
+// console.log(arrkvadrat([1,2,3,4,5,6,7,8,9,99]));
+
+
+//************************ 2-masala ************************
+
+// function musbatArr (arg){
+// let a = arg ; 
+// let b = [] ;
+// a.forEach(function(value){
+// if(value > 0 ){
+// b.push(value)
+// }
+// })
+// return b ;
+// }
+// console.log(musbatArr([1,2,-4,-3,6,-9,5]));
+
+
+// ********************* 3-masala ************************
+
+// function arr2x (arg){
+// let a = arg ; 
+// let b = [];
+// a.forEach(function(value){
+// b.push(value*2)
+// })
+// return b ;
+// }
+// console.log(arr2x([1,2,3,4,5,6,7,8,9]));
